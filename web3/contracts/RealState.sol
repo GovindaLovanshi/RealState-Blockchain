5f99113f39e77b42eca89be082748af1857d3fee
Govinda Lovanshi <govindalovanshi417@gmail.com>
2025-03-04 23:59:06 +0530

    Second day to update smart contract write some events mapping and function or struct

--- web3/contracts/RealState.sol
+++ web3/contracts/RealState.sol
@@ -22,9 +22,60 @@ contract RealEstate {
     mapping(uint256 => Property) private properties;
     uint256 public propertyIndex;
 
+    // events
+
+    event  PropertyListed(uint256 indexed id,address indexed owner ,uint256 price);
+    event PropertySold(uint256 indexed id,address indexed oldOwner,address indexed newOner,uint256 price);
+    event PropertyResold(uint256 indexed id,address indexed oldOwner,address indexed newOwner,uint256 price);
+
+    //Review
+
+    struct Review {
+        address reviewer;
+        uint256 productID;
+        uint256 rating;
+        string comments;
+        uint256 likes;
+    }
+
+    struct Product{
+        uint256 productId;
+        uint256 totalRating;
+        uint256 numReviews;
+    }
+
+    mapping(uint256 => Review[]) private reviews;
+    mapping(address => uint256[]) private userReviews;
+    mapping(uint256 => Product) private products;
+
+    uint256 public reviewCounter;
+
+    event ReviewAddes(uint256 indexed productId,address indexed reviewer,uint256 rating,string comment);
+    event ReviewLiked(uint256 indexed productId,uint256 indexed reviewIndex,address indexed liker,uint256 likes);
+
+
+
     // function in contract
 
-    function listProperty() external (uint256){}
+    function listProperty(address owner,uint256 price,string memory _propertyTitle,string memory _category,string memory _images,string memory _propertyAddress,string memory _description) external (uint256){
+        require(price > 0,"Price must be grater then 0");
+        
+        uint256 productId = propertyIndex++;
+        Property storage property = properties[productId];
+
+        property.productID;
+        property.owner = owner;
+        property.price = price;
+        property.propertyTitle = _propertyTitle;
+        property.category = _category;
+        property.images = _images;
+        property.propertyAddress = _propertyAddress;
+        property.description = _description;
+        
+        
+
+        event PropertyListed(productId,owner,price);
+    }
 
     function updateProperty() external (uint256){}
 
